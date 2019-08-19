81aab6a25da833f9ec46283d507240c039dfc543
Stuart Harland <stuartharland@stuarts-mbp.home>
2019-08-19 17:35:38 +0100

    Finalizing fixes

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
  server_names = [
-   for i in range(var.replicas) : format("%s-%d.%s.%s.svc.cluster.local:%d", kubernetes_stateful_set.memcached.metadata.0.name, kubernetes_service.memcached.metadata.0.name, var.namespace, var.memcache_port)
+   for i in range(var.replicas) : format("%s-%d.%s.%s.svc.cluster.local:%d", kubernetes_stateful_set.memcached.metadata.0.name, i, kubernetes_service.memcached.metadata.0.name, var.namespace, var.memcache_port)
  ]
 
 }
